49cc890e20b1efa8df22f9524cfb52b9f08f509b
NikitinSA <109169159+NikitinSA@users.noreply.github.com>
2022-11-27 18:45:16 +0300

    Task3

---
+++ Java_seminar_2/Task3.java
@@ -0,0 +1,22 @@
+package Java_seminar_2;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Task3 {
+    public static void main(String[] args){
+        try(FileWriter fw = new FileWriter("file.txt", true)){
+            String txt = "TEST";
+            StringBuilder str = new StringBuilder();
+
+            for (int i = 0; i < 100; i++){
+                str.append(txt);
+            }
+            fw.write(String.valueOf(str));
+            fw.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex);
+        }
+    }
+}
